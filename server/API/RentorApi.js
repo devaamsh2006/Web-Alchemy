d5bc401f236386457f974da113376bffc893070c
Aakruthi88 <aakruthigojuri88@gmail.com>
2025-03-29 14:06:08 +0530

    path changed

--- server/API/RentorApi.js
+++ server/API/RentorApi.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const RentModel = require("../models/rentModel"); // Import RentModel
+const RentModel = require("../schemas/rental"); // Import RentModel
 require("dotenv").config();
 
 const router = express.Router();
