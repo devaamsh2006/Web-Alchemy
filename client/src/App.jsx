4a04051c0a8a34520e786ee9c9e9e4b7d49191c3
Aakruthi88 <aakruthigojuri88@gmail.com>
2025-03-29 15:44:39 +0530

    routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,110 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import User from './components/User'
+import Rental from './components/Rental'
+import Farmer from './components/Farmer'
+import Home from './components/Common/Home'
+import Orders from './components/User/Orders'
+import Products from './components/User/Products'
+import Signout from './components/Common/Signout'
+
+import { createBrowserRouter, Navigate, RouterProvider } from 'react-router-dom'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const browserRouter=createBrowserRouter([
+  {
+    path:'/',
+    element:<RootLayout/>,
+    children:[
+      {
+        path:"",
+        element:<Home/>
+      },
+      {
+        path:"signin",
+        element:<Signin/>
+      },
+      {
+        path:"signup",
+        element:<Signup/>
+      },
+      {
+        path:"user-Profile/:email",
+        element:<User/>,
+        children:[
+          {
+            path:"products",
+            element:<Products/>
+          },
+          {
+            path:"",
+            element:< Navigate to="products"/>
+          },
+          {
+            path:"orders",
+            element:<Orders/>
+          },
+          {
+            path:"signout",
+            element:<Signout/>
+          }
+        ]
+      },
+      {
+        path:"admin-Profile/:email",
+        element:<Farmer/>,
+        children:[
+          {
+            path:"shop",
+            element:<Articles/>
+          },
+          {
+            path:"products",
+            element:<UserProfiles/>
+          },
+          {
+            path:"",
+            element:< Navigate to="shop"/>
+          },
+          {
+            path:"signout",
+            element:<Signout/>
+          },{
+            path:"orders",
+            element:<Orders/>
+          }
+        ]
+      },
+      {
+        path:"author-Profile/:email",
+        element:<Rental/>,
+        children:[
+          {
+            path:"rent",
+            element:<Articles/>
+          },
+          {
+            path:"orders",
+            element:<Articlesbyid/>
+          },
+          {
+            path:"",
+            element:< Navigate to="rent"/>
+          },
+          {
+            path:"signout",
+            element:<PostArticle/>
+          }
+        ]
+      }
+    ]
+  }
+])
+createRoot(document.getElementById('root')).render(
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+  <StrictMode>
+    <UserAuthor>
+        <RouterProvider router={browserRouter} />
+    </UserAuthor>
+  </StrictMode>,
+)
 
-export default App
