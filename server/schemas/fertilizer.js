deae2c6f441c042e319c440a330dea08b687d068
Aakruthi88 <aakruthigojuri88@gmail.com>
2025-03-29 14:22:08 +0530

    fertiseeds schemas added

---
+++ server/schemas/fertilizer.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const fertilizerSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    type: {
+        type: String,
+        enum: ["Organic", "Inorganic", "Biofertilizer", "Chemical", "Compost", "Other"],
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true,
+        min: 0
+    },
+    quantity: {
+        type: Number,
+        required: true,
+        min: 1
+    },
+    renatlid: {
+        type: String,
+        required: true
+    }
+});
+
+const Fertilizer = mongoose.model("Fertilizer", fertilizerSchema);
+module.exports = Fertilizer;
